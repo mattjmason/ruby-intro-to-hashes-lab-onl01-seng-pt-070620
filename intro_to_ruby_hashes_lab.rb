72e91b4d73d2eb17730eb8d269a523e9fe59c07d
Matt Mason <masonmj10@gmail.com>
2020-07-10 17:38:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,13 +21,13 @@ def monopoly_with_second_tier
 		monopoly = { :railroads => 
 		            { 
 		              :pieces => 4,
-		              :symbol => "symbol"
+		              :names => {}
+		              
+		            }
 		                          
 	
 		  
-		},
-		  
-		            :names => {}
+		
 		}
 
 end
